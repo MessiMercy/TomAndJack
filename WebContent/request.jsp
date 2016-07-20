0b90c698e382b85f71cf3acd37520d707104a703
Mercy <Mercy@192.168.0.122>
2016-07-21 00:04:47 +0800

    add a request

--- WebContent/request.jsp
+++ WebContent/request.jsp
@@ -1,3 +1,6 @@
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.PreparedStatement"%>
+<%@page import="java.sql.Connection"%>
 <%@page import="java.util.Arrays"%>
 <%@page import="java.util.Map.Entry"%>
 <%@page import="java.util.Iterator"%>
@@ -28,6 +31,22 @@
 	response.addHeader("u r sb", "sure");
 	response.addCookie(new Cookie("hehe","0.0"));
 %>
-<%@include file="index.jsp" %>
+<div><h1>i love you</h1></div>
+<%
+	String id = request.getParameter("id");
+	out.println(id);
+	Connection conn =(Connection) application.getAttribute("conn");
+	out.println("连接可用情况： "+(conn==null)+" "+(conn.isClosed()==true)+" "+(conn.isValid(10)==true)+" "+(conn.getSchema()));
+	PreparedStatement statement = conn.prepareStatement("SELECT * FROM post WHERE id=?");
+	statement.setLong(1, Long.valueOf(id));
+	ResultSet set = statement.executeQuery();
+	while(set.next()){
+		out.println("总共有"+set.getFetchSize()+"条信息");
+		for(int i=0;i<set.getFetchSize();i++){
+			out.println("第"+i+"条： "+set.getString(i+1));
+		}
+	}
+%>
+<%-- <%@include file="index.jsp" %>--%>
 </body>
 </html>
--- src/com/ConnListener.java
+++ src/com/ConnListener.java
@@ -12,22 +12,7 @@ public class ConnListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		try {
-			ServletContext application = arg0.getServletContext();
-			String url = application.getInitParameter("url");
-			String driver = application.getInitParameter("driver");
-			Class.forName(driver);
-			Connection conn = DriverManager.getConnection(url);
-			application.setAttribute("conn", conn);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
 
-	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
 		ServletContext application = arg0.getServletContext();
 		Connection conn = (Connection) application.getAttribute("conn");
 		if (conn != null) {
@@ -37,6 +22,27 @@ public class ConnListener implements ServletContextListener {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("application已关闭");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		try {
+			ServletContext application = arg0.getServletContext();
+			String url = application.getInitParameter("url");
+			String driver = application.getInitParameter("driver");
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url);
+			application.setAttribute("conn", conn);
+			if (conn != null && conn.isValid(10)) {
+				System.out.println("connection已建");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("检测到application启动！");
 	}
 
 }
